2f48020f5ee9655acadade661ac3e1f7ce2fda99
omajaqui <omajaqui@gmail.com>
2024-12-31 09:59:35 -0500

    firts commit

---
+++ src/thisGame.js
@@ -0,0 +1,28 @@
+function AutoVersionArchivosJuegos(){
+    return new Promise(resolve =>{
+        let numVersion = Math.random() * (1000 - 1) + 1;
+        numVersion = Math.round(numVersion,0);
+        let decimalVersion = Math.random() * (10 - 0) + 0;
+        decimalVersion = Math.round(decimalVersion,0);
+    
+        _autoVersionPhaser = `v${numVersion}.${decimalVersion}`; 
+        //console.log(_autoVersionPhaser)           
+    });        
+}
+
+function develop(){
+  let niveles = [
+    {nombre: "Laboratory", idJuego: 11, nivel: "1",disponible: 'S'},
+    {nombre: "Space", idJuego: 11, nivel: "2",disponible: 'S'}
+  ]
+  //guardar_datos_juegos(niveles);
+};
+
+function delay(time){
+    //console.log("se realiza delay de: "+time+" miliSegundos");
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(true);
+      }, parseInt(time));
+    });
+  }
